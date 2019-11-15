75fc1dea3ed959c6734e4c9140fbdc9db1f8d633
Jhonatan <45539807+Je0azul5@users.noreply.github.com>
2019-11-14 22:47:23 -0500

    Add files via upload

--- src/conexion/Conexion.java
+++ src/conexion/Conexion.java
@@ -10,26 +10,12 @@ import java.sql.*;
  * @author je0azul5
  */
 public class Conexion {
-    private static Connection cnx = null;
-   /*
-    public static Connection obtener() throws SQLException, ClassNotFoundException {
-      if (cnx == null) {
-         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            cnx = DriverManager.getConnection("jdbc:mysql://localhost:3306/ProyectoBases", "root", "Itaita07");
-         } catch (SQLException ex) {
-            throw new SQLException(ex);
-         } catch (ClassNotFoundException ex) {
-            throw new ClassCastException(ex.getMessage());
-         }
-      }
-      return cnx;
-   }
-  */ 
+     static Connection cnx = null;
     public static Connection conex(){
         try{
             Class.forName("com.mysql.jdbc.Driver");
-            cnx=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/ProyectoBases","root","Itaita07");
+            cnx=(Connection) DriverManager.getConnection("jdbc:mysql://localhost/proyecto","root","Itaita07");
+            System.out.println("Conectado");
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
@@ -45,6 +31,26 @@ public class Conexion {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         System.out.println(conex());
         
+        Statement statement = cnx.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * from Estudiante");
+
+            while (rs.next()) {
+
+                int id = rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                String apellido = rs.getString("apellido");
+                String edad = rs.getString("edad");
+                String carrera = rs.getString("carrera");
+                String semestre = rs.getString("semestre");
+                String telefono = rs.getString("telefono");
+                String bloqueo = rs.getString("bloqueo");
+
+                System.out.println(String.format("%d, %s %s, %s,%s, %s, %s,%s ", id, nombre, apellido, edad,carrera,semestre,telefono,bloqueo));
+            }
+
+            rs.close();
+            statement.close();
+            cnx.close();
        
     }
 }
